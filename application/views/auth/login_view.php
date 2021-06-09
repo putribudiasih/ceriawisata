806a59cd5b7e7dcdf1432fb1e5e263f76c9396d4
putribudiasih <putribudiasih18@gmail.com>
2021-06-10 01:00:48 +0700

    view login

--- application/views/auth/login_view.php
+++ application/views/auth/login_view.php
@@ -3,12 +3,16 @@
         <!-- Outer Row -->
         <div class="row justify-content-center">
 
-            <div class="col-lg-6">
+            <div class="col-xl-10 col-lg-12 col-md-9">
 
                 <div class="card o-hidden border-0 shadow-lg my-5">
                     <div class="card-body p-0">
+
                         <!-- Nested Row within Card Body -->
                         <div class="row">
+                            <div class="col-lg-7 d-none d-lg-block">
+                                <img src="<?= base_url('assets/img/login2.png'); ?>" class="card-img-top">
+                            </div>
                             <div class="col-lg">
                                 <div class="p-5">
                                     <div class="text-center">
@@ -42,6 +46,7 @@
                                     </div>
                                 </div>
                             </div>
+                        </div>
                         </div>
                     </div>
                 </div>
